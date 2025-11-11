ed60e037fec01e02db823b2af415552466dd1548
Anantha250 <ananthaxb@gmail.com>
2025-11-11 21:27:01 +0700

    new_version_avl

--- AVLTree/AVLTree5.py
+++ AVLTree/AVLTree5.py
@@ -1,40 +1,36 @@
 class BST:
     class BSTNode:
-        def __init__(self, data, left=None, right=None) -> None:
+        def __init__(self, data, left=None, right=None):
             self.data = data
             self.left = left
             self.right = right
 
-    def __init__(self, root=None) -> None:
-        self.root = root if root else None
+    def __init__(self, root=None):
+        self.root = root
 
     def search_subtree(self, root, key):
         if root is None:
             return None
         if key == root.data:
             return root
-        elif key < root.data:
+        if key < root.data:
             return self.search_subtree(root.left, key)
-        else:
-            return self.search_subtree(root.right, key)
+        return self.search_subtree(root.right, key)
 
     def insert(self, root, key):
-        return BST._insert(root, key)
-
-    def _insert(root, data):
         if root is None:
-            return BST.BSTNode(data)
-        if data < root.data:
-            root.left = BST._insert(root.left, data)
+            return BST.BSTNode(key)
+        if key < root.data:
+            root.left = self.insert(root.left, key)
         else:
-            root.right = BST._insert(root.right, data)
+            root.right = self.insert(root.right, key)
         return root
 
     def delete_subtree(self, root, key):
         if root is None:
             return None
         if root.data == key:
-            return None  
+            return None
         if key < root.data:
             root.left = self.delete_subtree(root.left, key)
         else:
@@ -50,79 +46,82 @@ class BST:
 
 class AVLTree:
     class AVLNode:
-        def __init__(self, data, left=None, right=None) -> None:
+        def __init__(self, data, left=None, right=None):
             self.data = data
             self.left = left
             self.right = right
-            self.height = 1 
+            self.height = 1
 
-    def __init__(self, root=None) -> None:
-        self.root = root if root else None
+    def __init__(self, root=None):
+        self.root = root
+
+    def get_height(self, node):
+        return 0 if node is None else node.height
+
+    def update_height(self, node):
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        return node
+
+    def get_balance(self, node):
+        return self.get_height(node.left) - self.get_height(node.right)
+
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+        y.left = z
+        z.right = T2
+        self.update_height(z)
+        self.update_height(y)
+        return y
+
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+        y.right = z
+        z.left = T3
+        self.update_height(z)
+        self.update_height(y)
+        return y
+
+    def _rebalance(self, node):
+        balance = self.get_balance(node)
+        if balance > 1:
+            if self.get_balance(node.left) < 0:
+                node.left = self.left_rotate(node.left)
+            return self.right_rotate(node)
+        if balance < -1:
+            if self.get_balance(node.right) > 0:
+                node.right = self.right_rotate(node.right)
+            return self.left_rotate(node)
+        return node
 
     def insert(self, root, data):
-        return self._insert(root, data)
-
-    def _insert(self, root, data):
         if root is None:
             return AVLTree.AVLNode(data)
         if data < root.data:
-            root.left = self._insert(root.left, data)
+            root.left = self.insert(root.left, data)
         else:
-            root.right = self._insert(root.right, data)
-
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        balance = self.get_balance(root)
-
-        if balance < -1:
-            if data < root.left.data:
-                return self.right_rotate(root)
-            else:
-                root.left = self.left_rotate(root.left)
-                return self.right_rotate(root)
-
-        if balance > 1:
-            if data > root.right.data:
-                return self.left_rotate(root)
-            else:
-                root.right = self.right_rotate(root.right)
-                return self.left_rotate(root)
-        return root
-
-    def left_rotate(self, root):
-        new_root = root.right
-        root.right = new_root.left
-        new_root.left = root
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        new_root.height = 1 + max(self.get_height(new_root.left), self.get_height(new_root.right))
-        return new_root
-
-    def right_rotate(self, root):
-        new_root = root.left
-        root.left = new_root.right
-        new_root.right = root
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        new_root.height = 1 + max(self.get_height(new_root.left), self.get_height(new_root.right))
-        return new_root
-
-    def get_height(self, root):
-        return 0 if root is None else root.height
-
-    def get_balance(self, root):
-        return self.get_height(root.right) - self.get_height(root.left)
-
-    def bst_to_avl(self, bst_root):
-        sorted_values = self.inorder_traversal(bst_root)
-        for val in sorted_values:
-            self.root = self.insert(self.root, val)
+            root.right = self.insert(root.right, data)
+        root = self.update_height(root)
+        return self._rebalance(root)
 
     def inorder_traversal(self, root):
         if root is None:
             return []
-        return (
-            self.inorder_traversal(root.left)
-            + [root.data]
-            + self.inorder_traversal(root.right)
-        )
+        return self.inorder_traversal(root.left) + [root.data] + self.inorder_traversal(root.right)
+
+    def _build_balanced_from_sorted(self, arr, l, r):
+        if l > r:
+            return None
+        m = (l + r) // 2
+        node = AVLTree.AVLNode(arr[m])
+        node.left = self._build_balanced_from_sorted(arr, l, m - 1)
+        node.right = self._build_balanced_from_sorted(arr, m + 1, r)
+        return self.update_height(node)
+
+    def bst_to_avl(self, bst_root):
+        vals = self.inorder_traversal(bst_root)
+        self.root = self._build_balanced_from_sorted(vals, 0, len(vals) - 1)
 
     def printTree90(self, root, indent=0):
         if root is not None:
@@ -133,8 +132,8 @@ class AVLTree:
 
 inp1, inp2 = input("Enter the val of tree and node to cut: ").split("/")
 bst = BST()
-for i in inp1.split():
-    bst.root = bst.insert(bst.root, int(i))
+for x in inp1.split():
+    bst.root = bst.insert(bst.root, int(x))
 
 print("Before cut:")
 bst.printTree90(bst.root)
