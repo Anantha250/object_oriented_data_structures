986a3727ba01bf8d1322dc59f5020499335e3996
Anantha250 <ananthaxb@gmail.com>
2025-12-03 21:26:58 +0700

    optimize_code

--- BinarySerachTree/searchchild.py
+++ BinarySerachTree/searchchild.py
@@ -19,39 +19,35 @@ class BST:
         return self.root
 
     def _insert(self, node, data):
-        if data < node.data:
-            if node.left is None:
-                node.left = Node(data)
+        while True:
+            if data < node.data:
+                if node.left is None:
+                    node.left = Node(data)
+                    break
+                node = node.left
             else:
-                self._insert(node.left, data)
-        else:
-            if node.right is None:
-                node.right = Node(data)
-            else:
-                self._insert(node.right, data)
+                if node.right is None:
+                    node.right = Node(data)
+                    break
+                node = node.right
 
-    def search_val(self,node, val) : 
-        if node != None :
-            
-            if node.data == val :
+    def search_val(self, node, val):
+        while node is not None:
+            if val == node.data:
                 return node
+            elif val < node.data:
+                node = node.left
+            else:
+                node = node.right
+        return None
 
-            isFound = self.search_val(node.left, val)
-            if isFound:
-                return isFound
-            isFound = self.search_val(node.right, val)
-            if isFound:
-                return isFound
-
-    def search_child(self, node, child = []) :
-        if node != None :
-            child.append(node.data)
-            self.search_child(node.left, child)
-            self.search_child(node.right, child)
-        return child            
+    def search_child(self, node):
+        if node is None:
+            return []
+        return [node.data] + self.search_child(node.left) + self.search_child(node.right)
         
     def printTree(self, node, level = 0):
-        if node != None:
+        if node is not None:
             self.printTree(node.right, level + 1)
             print('     ' * level, node)
             self.printTree(node.left, level + 1)
@@ -62,8 +58,9 @@ inp = [i for i in input("Enter the BST values and search value: ").split(", ")]
 
 bst_inp = [int(i) for i in inp[0].split()]
 print(f"Input: root = {bst_inp}, val = {inp[1]}")
-for i in bst_inp :
+
+for i in bst_inp:
     root = T.insert(i)
 
-new_root = T.search_val(root,int(inp[1]))
-print("Output:",T.search_child(new_root))
+new_root = T.search_val(root, int(inp[1]))
+print("Output:", T.search_child(new_root))
