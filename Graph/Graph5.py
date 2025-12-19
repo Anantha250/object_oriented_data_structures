5ff37aaf30384468e8b515b30cb37cbd15f9186e
Anantha250 <ananthaxb@gmail.com>
2025-12-19 22:51:44 +0700

    Adjust

--- Graph/Graph5.py
+++ Graph/Graph5.py
@@ -1,16 +1,8 @@
 class Graph:
-    def __init__(self):
-        self.edge = [[] for _ in range(1000)]
-        self.root = [i for i in range(1000)]
-        self.color = [0 for _ in range(1000)]
-
-    def parent(self, u):
-        if self.root[u] != u:
-            self.root[u] = self.parent(self.root[u])
-        return self.root[u] 
-    
-    def union(self, u, v):
-        self.root[self.parent(u)] = self.parent(v)
+    def __init__(self, n=1000):
+        self.n = n
+        self.edge = [[] for _ in range(n)]
+        self.color = [0] * n
 
     def add(self, u, v):
         self.edge[u].append(v)
@@ -20,29 +12,25 @@ class Graph:
             return True
         if self.color[u] == 2:
             return False
-        
+
         self.color[u] = 1
         for v in self.edge[u]:
             if self.dfs(v):
                 return True
         self.color[u] = 2
-    
+        return False
+
     def check(self):
-        for u in range(1000):
-            if self.dfs(u):
+        for u in range(self.n):
+            if self.color[u] == 0 and self.dfs(u):
                 return True
         return False
 
 
-
-
 inp = [[int(k) for k in e.split()] for e in input("Enter : ").split(',')]
 g = Graph()
-flag = False
+
 for u, v in inp:
     g.add(u, v)
 
-if g.check():
-    print("Graph has a cycle")
-else: 
-    print("Graph has no cycle")
+print("Graph has a cycle" if g.check() else "Graph has no cycle")
